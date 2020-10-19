03c019703275f7b6719e6ef0014e79381b101fa2
rpalermo15 <rpalermo15@vera.com.uy>
2020-10-19 20:55:05 -0300

    Uso de menus

--- app/src/main/java/com/infinitid/veterinaria/MainActivity.java
+++ app/src/main/java/com/infinitid/veterinaria/MainActivity.java
@@ -1,7 +1,8 @@
 package com.infinitid.veterinaria;
 
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.widget.Toolbr;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,6 +10,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
@@ -45,6 +48,28 @@ public class MainActivity extends AppCompatActivity {
 
         inicializarAdaptador();
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_opciones, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.mContacto:
+                //Intent intent = new Intent(this, ActivityAbout.class);
+                //startActivity(intent);
+                break;
+            case R.id.mAcercaDe:
+                //Intent intent1 = new Intent(this, ActivitySettings.class);
+                //startActivity(intent1);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void inicializarAdaptador() {
